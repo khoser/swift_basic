1f0fb072b0fb98d266a2682c2fbf68eee422daaf
Oleg <ktuuuu@gmail.com>
2021-08-25 00:01:44 +0400

    Task 1

--- swift_basic/main.swift
+++ swift_basic/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+//task 1
+let a: Double = 1
+let b: Double = 0
+let c: Double = -1
 
+let d = b * b - 4 * a * c
+
+if d < 0 {
+    print("Нет решения - дискриминант отрицательный: \(d)")
+} else if a == 0 {
+    if b == 0 {
+        print("Решения нет")
+    } else {
+        let x = -c/b
+        print("Это не квадратное уравнение. x = \(x)")
+    }
+} else if d == 0 {
+    let x: Double = 0 - b / 2 / a
+    print("x = \(x)")
+} else {
+    let x1: Double = (0 - b + sqrt(d)) / 2 / a
+    let x2: Double = (0 - b - sqrt(d)) / 2 / a
+    print("D = \(d)\nx1 = \(x1)\nx2 = \(x2)")
+}
