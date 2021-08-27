7c7ea4ac7f6c26a38e72b0baff28c289b1b8222e
Oleg Khavroshin <ktuuuu@gmail.com>
2021-08-27 23:46:53 +0400

    Task 1

--- swift_basic/main.swift
+++ swift_basic/main.swift
@@ -4,8 +4,16 @@
 //
 //  Created by Олег on 24.08.2021.
 //
+//lesson 2
 
 import Foundation
 
-print("Hello, World!")
+
+//Task 1
+func isEven(_ val : Int) -> Bool {
+    return val % 2 == 0
+}
+
+print(isEven(2))
+print(isEven(3))
 
